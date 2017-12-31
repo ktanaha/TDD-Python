a1b2cf9839191972773dea706140e1e4e7c5ee60
ktanaha <moutoku1984@gmail.com>
2017-12-31 12:58:56 +0900

    add static method

--- money/money.py
+++ money/money.py
@@ -5,3 +5,13 @@ class Money(object):
 
     def __eq__(self, other: "Money") -> bool:
         return (self.amount == other.amount) and ( self.__class__.__name__ == other.__class__.__name__)
+    
+    @staticmethod
+    def doller(amount: int) -> "Money":
+        from .doller import Doller
+        return Doller(amount)
+    
+    @staticmethod
+    def franc(amount: int) -> "Money":
+        from .franc import Franc
+        return Franc(amount)
